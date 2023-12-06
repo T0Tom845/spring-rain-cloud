36049c7ba447e0c9ac580d9eb60baab899239715
T0Tom <lapinkirill.apple@mail.ru>
2023-12-07 00:30:59 +1200

    init commit

---
+++ src/main/java/totom/example/springraincloud/SpringRainCloudApplication.java
@@ -0,0 +1,13 @@
+package totom.example.springraincloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRainCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRainCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/totom/example/springraincloud/SpringRainCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package totom.example.springraincloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRainCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
